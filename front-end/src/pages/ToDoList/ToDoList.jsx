cdde88ef412cf50bacf8d5ee0b6f3b42e317fa3f
Sara Bevilacqua <sarabev6@gmail.com>
2022-12-03 12:11:54 +0100

    feat-user can add/delete tasks

--- front-end/src/pages/ToDoList/ToDoList.jsx
+++ front-end/src/pages/ToDoList/ToDoList.jsx
@@ -1,11 +1,11 @@
+import React, { useState } from "react";
 import "./ToDoList.css";
 import "../../hooks/useGetToDoList";
 //import useGetToDoList from "../../hooks/useGetToDoList";
 
 const ToDoList = () => {
-  //const { loading, data, refresh } = useGetToDoList;
-
-  const todoData = [
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([
     {
         "task": "cuisine à faire",
         "done": false,
@@ -17,18 +17,50 @@ const ToDoList = () => {
     {
       "task": "sport à faire",
       "done": false,
-  }
-  ];
+    }
+  ]);
+  console.log(taskList);
+
+  const addTask = () => {
+    if (task.length) {
+      const newtaskList = [...taskList, { task, "done": false }]
+      setTaskList(newtaskList);
+    };
+  };
+
+  const removeTask = index => {
+    const newTaskList = [...taskList];
+    newTaskList.splice(index, 1);
+    setTaskList(newTaskList);
+  };
   
   return(
     <>
         <main className="todo">
           <section className="todo-section">
+
+            {/* Add task */}
+            <div className="add-to-do">
+              {/*ajouter après label invisible*/}
+              <input 
+                type="text" 
+                placeholder="Write new task..."
+                onChange={e => setTask(e.target.value)}
+              />
+              <button onClick={() => addTask( )}>Add</button>
+            </div>
+
+            {/*table*/}
             <table>
               <tbody>
-                {todoData.map((element,index) =>(
+                {taskList.map((element,index) =>(
                   <tr key={index}>
-                    <td><label><input type="checkbox" />{element.task}</label></td>
+                    <td>
+                      <label>
+                        <input type="checkbox" />{element.task}
+                      </label>
+                      <button onClick={e => removeTask(index)}>Delete</button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
