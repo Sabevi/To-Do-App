337f470448a43b3c8846ee8771a82881048056f5
Sara Bevilacqua <sarabev6@gmail.com>
2023-06-04 17:10:23 +0200

    feat - added CI on Github

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example support/e2e.ts is processed and
+// This example support/e2e.js is processed and
 // loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
@@ -18,7 +18,3 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-Cypress.on('uncaught:exception', (err) => {
-  return false;
-});
--- front-end/cypress/e2e/addTask.cy.ts
+++ front-end/cypress/e2e/addTask.cy.ts
@@ -1,15 +1,14 @@
 describe("Add task", () => {
   before(() => {
     cy.visit("http://localhost:3000");
-  });
+});
 
   it("Should add a task", () => {
     cy.fixture("taskList").then((taskList) =>{
-      cy.get("[data-cy='add-task']").type(taskList[0].task);
+      cy.get("[data-cy='add-task']", { timeout: 45000 }).type(taskList[0].task);
       cy.get("[data-cy='submit']").click();
       cy.contains(taskList[0].task).should("exist");
     })
   });
 });
-
 
