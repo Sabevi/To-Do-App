7ebe364c69e8a4b76db7553c53156643520f9d09
Sara Bevilacqua <sarabev6@gmail.com>
2023-06-06 21:46:34 +0200

    fix-CI tests [still dont work]

--- front-end/cypress/e2e/addTask.cy.ts
+++ front-end/cypress/e2e/addTask.cy.ts
@@ -1,11 +1,8 @@
 describe("Add task", () => {
-  before(() => {
+ it("Should add a task", () => {
     cy.visit("/");
-});
-
-  it("Should add a task", () => {
     cy.fixture("taskList").then((taskList) =>{
-      cy.get("[data-cy='add-task']", { timeout: 45000 }).type(taskList[0].task);
+      cy.get("[data-cy='add-task']").type(taskList[0].task);
       cy.get("[data-cy='submit']").click();
       cy.contains(taskList[0].task).should("exist");
     })
