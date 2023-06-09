690fd21448705bf5d53163f11155824b7866bdb5
Sara Bevilacqua <sarabev6@gmail.com>
2023-06-09 13:37:30 +0200

    fix - Cypress errors [Github Actions]

--- front-end/cypress/e2e/addTask.cy.ts
+++ front-end/cypress/e2e/addTask.cy.ts
@@ -2,8 +2,9 @@ describe("Add task", () => {
  it("Should add a task", () => {
     cy.visit("/");
     cy.fixture("taskList").then((taskList) =>{
+      expect(taskList[0].task).to.eq('Do the laundry');
       cy.get("[data-cy='add-task']").type(taskList[0].task);
-      cy.get("[data-cy='submit']").click();
+      cy.get("[data-cy='submit']").click().wait(0);
       cy.contains(taskList[0].task).should("exist");
     })
   });
